dea47bd07e97200181aec410742994be64715717
Suraj Deuja <deujasuraj77@gmail.com>
2014-02-22 10:16:34 -0700

    toggling led

---
+++ demos/toggling-led/src/main.c
@@ -0,0 +1,16 @@
+/**
+ * A simple project that does not do anything
+ * interesting.
+ * Author : Suraj Deuja
+ * Email  : surajdeuja@u.boisestate.edu
+ */
+
+#include <stm32f4xx_conf.h>
+
+int main(void) {
+
+	while(1);
+
+	return 0;
+}
+

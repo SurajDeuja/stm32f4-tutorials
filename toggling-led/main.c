3ada671b016a6a3490759df714022f44f9a67ae8
Suraj Deuja <deujasuraj77@gail.com>
2014-06-25 21:56:12 -0600

    added led

--- toggling-led/main.c
+++ toggling-led/main.c
@@ -1,16 +1,20 @@
-/**
- * A simple project that does not do anything
- * interesting.
- * Author : Suraj Deuja
- * Email  : surajdeuja@u.boisestate.edu
- */
+#include "stm32f4xx_conf.h"
 
-#include <stm32f4xx_conf.h>
+#include "stdio.h"
 
-int main(void) {
+int main(int argc, char *argv[])
+{
+    	GPIO_InitTypeDef gpioInitStruct;
+    	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	while(1);
+    	gpioInitStruct.GPIO_Pin   = GPIO_Pin_15 | GPIO_Pin_14;
+    	gpioInitStruct.GPIO_Mode  = GPIO_Mode_OUT;
+    	gpioInitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    	gpioInitStruct.GPIO_OType = GPIO_OType_PP;
+    	gpioInitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+    	GPIO_Init(GPIOD, &gpioInitStruct);
+
+    	GPIOD->BSRRL = 0xF000;
 
 	return 0;
 }
-
